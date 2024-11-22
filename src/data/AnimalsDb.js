4e8967105cc441433c6c73c6a6d206b37487022f
Kaweesha-Malshani <537tharu@gmail.com>
2024-11-22 18:35:57 +0530

    edit animaldb.js

---
+++ src/data/AnimalsDb.js
@@ -0,0 +1,10 @@
+const animals = [
+    { name: "Cat", image: "/images/cat.png" },
+    { name: "Dog", image: "/images/dog.png" },
+    { name: "Elephant", image: "/images/elephant.png" },
+    { name: "Lion", image: "/images/lion.png" },
+    // Add the rest of the 16 animals here
+  ];
+  
+  export default animals;
+  
