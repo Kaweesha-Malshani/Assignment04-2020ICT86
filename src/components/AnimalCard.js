b8747a843e09ebda1ca40bf074874029200c3537
Kaweesha-Malshani <537tharu@gmail.com>
2024-11-23 09:00:46 +0530

    delete game.js

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/Game.css";
+//import animals from "../data/AnimalsDb";
 
 const AnimalCard = ({ animal, onClick }) => {
   return (
     <div className="animal-card" onClick={onClick}>
-      <img src={animal.image} alt={animal.name} className="animal-image" />
+      <img src={`/images/${animal.image}`} alt={animal.name} className="animal-image" />
     </div>
   );
 };
--- src/data/AnimalsDb.js
+++ src/data/AnimalsDb.js
@@ -1,22 +1,22 @@
 const animals = [
-    { id: 1, name: "Dog", image: "dog.jpg" },
-    { id: 2, name: "Cat", image: "cat.jpg" },
-    { id: 3, name: "Lion", image: "lion.jpg" },
-    { id: 4, name: "Tiger", image: "tiger.jpg" },
-    { id: 5, name: "Elephant", image: "elephant.jpg" },
-    { id: 6, name: "Monkey", image: "monkey.jpg" },
-    { id: 7, name: "Zebra", image: "zebra.jpg" },
-    { id: 8, name: "Bear", image: "bear.jpg" },
-    { id: 9, name: "Giraffe", image: "giraffe.jpg" },
-    { id: 10, name: "Fox", image: "fox.jpg" },
-    { id: 11, name: "Rabbit", image: "rabbit.jpg" },
-    { id: 12, name: "Horse", image: "horse.jpg" },
-    { id: 13, name: "Deer", image: "deer.jpg" },
-    { id: 14, name: "Kangaroo", image: "kangaroo.jpg" },
-    { id: 15, name: "Penguin", image: "penguin.jpg" },
-    { id: 16, name: "Panda", image: "panda.jpg" },
-  ];
-  
-  export default animals;
-  
+  { id: 1, name: "Dog", image: "dog.jpg" },
+  { id: 2, name: "Cat", image: "cat.jpg" },
+  { id: 3, name: "Lion", image: "lion.jpg" },
+  { id: 4, name: "Tiger", image: "tiger.jpg" },
+  { id: 5, name: "Elephant", image: "elephant.jpg" },
+  { id: 6, name: "Monkey", image: "monkey.jpg" },
+  { id: 7, name: "Zebra", image: "zebra.jpg" },
+  { id: 8, name: "Bear", image: "bear.jpg" },
+  { id: 9, name: "Giraffe", image: "giraffe.jpg" },
+  { id: 10, name: "Fox", image: "fox.jpg" },
+  { id: 11, name: "Rabbit", image: "rabbit.jpg" },
+  { id: 12, name: "Horse", image: "horse.jpg" },
+  { id: 13, name: "Deer", image: "deer.jpg" },
+  { id: 14, name: "Kangaroo", image: "kangaroo.jpg" },
+  { id: 15, name: "Penguin", image: "penguin.jpg" },
+  { id: 16, name: "Panda", image: "panda.jpg" },
+];
+
+export default animals;
+
   
